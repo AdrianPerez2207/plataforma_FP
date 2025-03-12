835f694475895b7ff10f91d377013ae8b52a2dc6
AdrianPerez2207 <aperezsanchez441@gmail.com>
2025-03-12 12:49:13 +0100

    registro arreglado

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -30,21 +30,39 @@ class RegisteredUserController extends Controller
     public function store(Request $request): RedirectResponse
     {
         $request->validate([
-            'name' => ['required', 'string', 'max:255'],
-            'email' => ['required', 'string', 'lowercase', 'email', 'max:255', 'unique:'.User::class],
-            'password' => ['required', 'confirmed', Rules\Password::defaults()],
+            'name' => 'required|string|max:255',
+            'last_name' => 'required|string|max:255',
+            'dni' => 'required|unique:users|max:255',
+            'phone_number' => 'required|string|max:255',
+            'address' => 'required|string|max:255',
+            'country' => 'required|string|max:255',
+            'email' => 'required|unique:users|max:255',
+            'password' => 'required|string|max:255',
+            'password_confirmation' => 'required|string|max:255',
         ]);
-
+        //Verificamos si ya existe un usuario con ese DNI
+        if (User::where('dni', $request->dni)->exists()){
+            return redirect()->back()->withErrors(['error-msg' => 'Ya existe un usuario con ese DNI']);
+        }
+        //Creamos el usuario
         $user = User::create([
             'name' => $request->name,
+            'last_name' => $request->last_name,
+            'dni' => $request->dni,
+            'phone_number' => $request->phone_number,
+            'address' => $request->address,
+            'country' => $request->country,
             'email' => $request->email,
             'password' => Hash::make($request->password),
         ]);
+        if ($user != null){
+            event(new Registered($user));
 
-        event(new Registered($user));
+            Auth::login($user);
 
-        Auth::login($user);
-
-        return redirect(route('welcome', absolute: false));
+            return redirect(route('welcome', absolute: false))->with('msg', 'Usuario creado correctamente');
+        } else {
+            return redirect()->back()->withErrors(['error-msg' => 'Error al crear el usuario']);
+        }
     }
 }
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,4 +1,19 @@
 <x-guest-layout>
+    @if(session('msg'))
+        <div class="alert alert-success bg-green-400 text-white w-100 text-center text-2xl rounded-md mb-4">
+            {{ session('msg') }}
+        </div>
+    @endif
+
+    @if($errors->any())
+        <div class="alert alert-danger bg-red-400 text-white w-100 text-center text-2xl rounded-md mb-4">
+            <ul>
+                @foreach($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
     <form method="POST" action="{{ route('register') }}">
         @csrf
 
